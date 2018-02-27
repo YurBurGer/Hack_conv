0ee7dc5e96c555c8e9547c6e8d5cc9b8b64741de
Ramil Safin <ramilsafnab1996@gmail.com>
2018-02-27 03:25:43 +0300

    New indexes for red object

--- vision.py
+++ vision.py
@@ -6,6 +6,7 @@ TRACKING_IMAGE_WINDOW = "Tracking"
 HSV_IMAGE_WINDOW = "HSV"
 FILTERED_IMAGE_WINDOW = "HSV (Filtered)"
 VIDEO_SOURCE = 1
+
 TRACKER_TYPE = "KCF"
 
 video = cv2.VideoCapture()
@@ -15,14 +16,15 @@ if not video.isOpened():
 
 cv2.namedWindow(HSV_CONTROL_WINDOW, cv2.WINDOW_AUTOSIZE)
 
+
 class Params:
     def __init__(self):
-        self.lowH  = 8
-        self.highH = 24
-        self.lowS  = 135
+        self.lowH = 179
+        self.highH = 179
+        self.lowS = 133
         self.highS = 255
-        self.lowV  = 203
-        self.highV = 255
+        self.lowV = 22
+        self.highV = 128
 
 
 params = Params()
@@ -30,19 +32,27 @@ params = Params()
 
 def onLowHChanged(value):
     params.lowH = value
+
+
 def onHighHChanged(value):
     params.highH = value
+
+
 def onLowSChanged(value):
     params.lowS = value
+
+
 def onHighSChanged(value):
     params.highS = value
+
+
 def onLowVChanged(value):
     params.lowV = value
+
+
 def onHighVChanged(value):
     params.HighV = value
 
-def nothing():
-    pass
 
 cv2.createTrackbar("LowH", HSV_CONTROL_WINDOW, params.lowH, 179, onLowHChanged)
 cv2.createTrackbar("HighH", HSV_CONTROL_WINDOW, params.highH, 179, onHighHChanged)
@@ -51,11 +61,15 @@ cv2.createTrackbar("HighS", HSV_CONTROL_WINDOW, params.highS, 255, onHighSChange
 cv2.createTrackbar("LowV", HSV_CONTROL_WINDOW, params.lowV, 255, onLowVChanged)
 cv2.createTrackbar("HighV", HSV_CONTROL_WINDOW, params.highV, 255, onHighVChanged)
 
-while (video.isOpened()):
+while video.isOpened():
     ret, frame = video.read()
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
-    thresholded = cv2.inRange(hsv, np.array([params.lowH, params.lowS, params.lowV]), np.array([params.highH, params.highS, params.highV]))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    thresholded = cv2.inRange(hsv, np.array([params.lowH, params.lowS, params.lowV]),
+                              np.array([params.highH, params.highS, params.highV]))
+
+    thresholded = cv2.dilate(thresholded, None, iterations=5)
+    thresholded = cv2.erode(thresholded, None, iterations=5)
 
     moments = cv2.moments(thresholded)
 
@@ -70,10 +84,9 @@ while (video.isOpened()):
     if cv2.waitKey(1) == 27:
         break
 
+
     def get_coordinanes():
         return [posX, posY]
 
 video.release()
 cv2.destroyAllWindows()
-
-
